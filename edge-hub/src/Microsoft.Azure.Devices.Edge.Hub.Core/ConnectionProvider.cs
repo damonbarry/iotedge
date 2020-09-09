8e81c5f18de811b5a9c88b96c86383e3fdb6c5a6
Damon Barry <damonb@microsoft.com>
2020-09-09 11:28:35 -0700

    Add a trace when Edge hub receives a message

--- edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/ConnectionProvider.cs
+++ edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/ConnectionProvider.cs
@@ -6,6 +6,8 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
     using Microsoft.Azure.Devices.Edge.Hub.Core.Device;
     using Microsoft.Azure.Devices.Edge.Hub.Core.Identity;
     using Microsoft.Azure.Devices.Edge.Util;
+    using OpenTelemetry;
+    using OpenTelemetry.Trace;
 
     public class ConnectionProvider : IConnectionProvider
     {
@@ -13,16 +15,28 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
         readonly IEdgeHub edgeHub;
         readonly TimeSpan messageAckTimeout;
 
+        readonly TracerProvider tracerProvider;
+
         public ConnectionProvider(IConnectionManager connectionManager, IEdgeHub edgeHub, TimeSpan messageAckTimeout)
         {
             this.connectionManager = Preconditions.CheckNotNull(connectionManager, nameof(connectionManager));
             this.edgeHub = Preconditions.CheckNotNull(edgeHub, nameof(edgeHub));
             this.messageAckTimeout = messageAckTimeout;
+            this.tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddSource("Microsoft.Azure.Devices.Edge.Hub")
+                .AddOtlpExporter()
+                .Build();
         }
 
         public Task<IDeviceListener> GetDeviceListenerAsync(IIdentity identity, Option<string> modelId)
         {
-            IDeviceListener deviceListener = new DeviceMessageHandler(Preconditions.CheckNotNull(identity, nameof(identity)), this.edgeHub, this.connectionManager, this.messageAckTimeout, modelId);
+            IDeviceListener deviceListener = new DeviceMessageHandler(
+                Preconditions.CheckNotNull(identity, nameof(identity)),
+                this.edgeHub,
+                this.connectionManager,
+                this.tracerProvider,
+                this.messageAckTimeout,
+                modelId);
             return Task.FromResult(deviceListener);
         }
 
@@ -33,6 +47,7 @@ namespace Microsoft.Azure.Devices.Edge.Hub.Core
             if (disposing)
             {
                 this.edgeHub?.Dispose();
+                this.tracerProvider?.Dispose();
             }
         }
     }
